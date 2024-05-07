daab952fad0ee26caf680d8f761d85d4ebc3c4c4
Samuel Souza Lacerda <sammolacerda@gmail.com>
2024-05-07 15:38:41 -0300

    Cria func buscarServidores

--- app/app.go
+++ app/app.go
@@ -1,30 +1,36 @@
 package app
+
 import (
-	
 	"fmt"
 	"log"
 	"net"
 
 	"github.com/urfave/cli"
 )
+
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicacao de linha de comando"
 	app.Usage = "Busca IPs e Nomes de Servidores na internet"
+	flag :=[]cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs na internet",
-			Flags:[]cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPs na internet",
+			Flags:  flag,
 			Action: buscarIps,
+		}, 
+		{
+			Name:   "servidores",
+			Usage:  "Busca Nomes de Servidores na internet",
+			Flags:  flag,
+			Action: buscarServidores,
 		},
-		
 	}
 	return app
 }
@@ -38,5 +44,16 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+func buscarServidores (c *cli.Context){
+	host := c.String("host")
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
